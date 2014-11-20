e9ae13eb39638185a94beaa15213bf1d0553455a
Carey Estes <cestes@sva.edu>
2014-11-19 19:43:04 -0500

    Adding WP core

---
+++ wp-content/themes/two-rivers/front-page.php
@@ -0,0 +1,8 @@
+<?php
+get_header(); ?>
+	<div id="primary" class="site-content homepage">
+		<div id="content" role="main">
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+<?php get_footer(); ?>
