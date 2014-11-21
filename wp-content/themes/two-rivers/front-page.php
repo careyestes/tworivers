35ee58832c79c7546e5107c019b227f011f0fd73
Carey Estes <cestes@sva.edu>
2014-11-20 22:17:25 -0500

    WIP

--- wp-content/themes/two-rivers/front-page.php
+++ wp-content/themes/two-rivers/front-page.php
@@ -3,5 +3,12 @@ get_header();
 get_template_part('header','carousel');
 get_template_part( 'brand-thumbs');
 ?>
+
+<?php // Begin parallax ?>
+<section class="storyboardLevel familyOfBrands">
+	<h3>TRC Family of Brands</h3>
+	<article class="copy"><p>Lorem ipsum Et nulla sit nisi cupidatat Ut ut eu eu deserunt adipisicing irure fugiat non exercitation ea fugiat labore ut Ut ut minim nulla consequat ullamco magna id aute consequat nostrud irure in cupidatat eu do in laborum do adipisicing labore eu Ut anim sit fugiat in Excepteur nostrud consectetur et minim ea velit qui reprehenderit Duis in id elit sit laboris ea occaecat sed officia velit anim sunt dolore in in proident ea id culpa eu ut voluptate esse nostrud occaecat labore minim in ad dolore Ut adipisicing aliquip culpa laboris dolor ex Ut ad exercitation proident commodo culpa occaecat.</p>
+	</article>
+</section>
 	
 <?php get_footer(); ?>
