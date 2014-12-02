9edb93a83be98629dcc1b37d730e64912bea2fb3
Carey Estes <cestes@sva.edu>
2014-12-01 23:02:09 -0500

    WIP

--- wp-content/themes/two-rivers/brand-lids.php
+++ wp-content/themes/two-rivers/brand-lids.php
@@ -31,28 +31,28 @@
 						    		if($type['slug'] == "coffee" && $lid['brand_flavor_lid_type']) {
 								    		if($lid['brand_flavor_lid_type'] == 'light') {
 								    			$lightRoastsArray[] = array( 
-								    				'url' => $lid['flavor_image'], 
+								    				'url' => $lid['flavor_lid_image'], 
 								    				'title' => $lid['brand_flavor_title'],
 								    				'excerpt' => $lid['brand_flavor_excerpt'],
 								    				'slug' => get_the_permalink()
 								    			);
 								    		} elseif($lid['brand_flavor_lid_type'] == 'medium') {
 								    			$mediumRoastsArray[] = array( 
-								    				'url' => $lid['flavor_image'], 
+								    				'url' => $lid['flavor_lid_image'], 
 								    				'title' => $lid['brand_flavor_title'],
 								    				'excerpt' => $lid['brand_flavor_excerpt'],
 								    				'slug' => get_the_permalink()
 								    			);
 								    		} elseif($lid['brand_flavor_lid_type'] == 'dark') {
 								    			$darkRoastsArray[] = array( 
-								    				'url' => $lid['flavor_image'],
+								    				'url' => $lid['flavor_lid_image'],
 								    				'title' => $lid['brand_flavor_title'], 
 								    				'excerpt' => $lid['brand_flavor_excerpt'],
 								    				'slug' => get_the_permalink()
 								    			);
 								    		} elseif($lid['brand_flavor_lid_type'] == 'flavored') {
 								    			$flavoredRoastsArray[] = array( 
-								    				'url' => $lid['flavor_image'], 
+								    				'url' => $lid['flavor_lid_image'], 
 								    				'title' => $lid['brand_flavor_title'],
 								    				'excerpt' => $lid['brand_flavor_excerpt'],
 								    				'slug' => get_the_permalink()
@@ -74,6 +74,7 @@
 				  <?php wp_reset_postdata(); ?>
 
 				<?php if($lightRoastsArray && count($lightRoastsArray) > 0): ?>
+						<?php shuffle($lightRoastsArray) ?>
 						<section class="roastRow lightRow">
 							<h5>Light</h5>
 							<?php $l = 1; foreach($lightRoastsArray as $lid): ?>
@@ -91,6 +92,7 @@
 
 
 				<?php if($mediumRoastsArray && count($mediumRoastsArray) > 0): ?>
+					<?php shuffle($mediumRoastsArray) ?>
 						<section class="roastRow mediumRow">
 							<h5>Medium</h5>
 							<?php $l = 1; foreach($mediumRoastsArray as $lid): ?>
@@ -108,6 +110,7 @@
 
 
 				<?php if($darkRoastsArray && count($darkRoastsArray) > 0): ?>
+					<?php shuffle($darkRoastsArray) ?>
 						<section class="roastRow darkRow">
 							<h5>Dark</h5>
 							<?php $l = 1; foreach($darkRoastsArray as $lid): ?>
@@ -125,6 +128,7 @@
 				
 
 				<?php if($flavoredRoastsArray && count($flavoredRoastsArray) > 0): ?>
+					<?php shuffle($flavoredRoastsArray) ?>
 						<section class="roastRow flavoredRow">
 							<h5>Flavored</h5>
 							<?php $l = 1; foreach($flavoredRoastsArray as $lid): ?>
@@ -141,6 +145,7 @@
 
 
 				<?php if($otherTypeArray && count($otherTypeArray) > 0): ?>
+					<?php shuffle($otherTypeArray) ?>
 						<section class="roastRow <?php echo $type ?>Row">
 						<h5></h5>
 							<?php $l = 1; foreach($otherTypeArray as $lid): ?>
