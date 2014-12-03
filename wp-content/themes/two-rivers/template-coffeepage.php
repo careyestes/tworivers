2c87d7d544eb6d570276a5544eb0c5ac3e9c6242
Carey Estes <carey.estes@utk.edu>
2014-12-03 17:56:25 -0500

    Added coffee template

---
+++ wp-content/themes/two-rivers/template-coffeepage.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Template Name: Coffee Template
+ * Description: Shows all coffee brands
+ *
+ * @package WordPress
+ * @subpackage utresponsive
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="site-content ">
+		<div id="content" class="brandContainer" role="main">
+
+			<?php 
+				$coffeeArgs = array(
+					'post_type' => 'tr_brands',
+					'posts_per_page' => -1,
+					'meta_key' => 'brand_categories',
+					'meta_value' => 'coffee'
+				);
+				$coffeeQuery = new WP_Query($coffeeArgs);
+
+
+				while ( $coffeeQuery->have_posts() ) : $coffeeQuery->the_post(); ?>
+				<article class="brandThumbnail">
+					<?php $mainThumb = get_field('brand_image_main'); ?>
+						<img class="brandImage" src="<?php echo $mainThumb['url'] ?>" alt="<?php echo $mainThumb['title'] ?>">
+						<h2><?php the_title(); ?></h2>
+				</article>
+				
+			<?php endwhile; // end of the loop. ?>
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
